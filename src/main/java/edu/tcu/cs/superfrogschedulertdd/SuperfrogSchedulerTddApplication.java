d0faadf2d6d310dc7e4206c533c1d8204c88925c
Washingtonwei <weibingyang@gmail.com>
2023-03-03 15:02:06 -0600

    This is the first commit.

---
+++ src/main/java/edu/tcu/cs/superfrogschedulertdd/SuperfrogSchedulerTddApplication.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.superfrogschedulertdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperfrogSchedulerTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperfrogSchedulerTddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/tcu/cs/superfrogschedulertdd/SuperfrogSchedulerTddApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.superfrogschedulertdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperfrogSchedulerTddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
